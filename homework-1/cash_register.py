b22799dc38f363d9b43c6659993fd1151986f13d
ruthie02 <maryruthbongon@gmail.com>
2022-10-14 16:19:09 +0200

    Submitted homework with answers

--- homework-1/cash_register.py
+++ homework-1/cash_register.py
@@ -1,50 +1,66 @@
-"""
-
-TASK 1
-
-Write a class to represent a Cash Register.
-You class should keep the state of price total and purchased items
-
-Below is a starter code:
-------------------------
-1. you can add new variables and function if you want to
-2. you will NEED to add accepted method parameters where required.
-For example, method add_item probably accepts some kind of an item?..
-3. You will need to write some examples of how your code works.
-
-"""
-
-
 class CashRegister:
 
     def __init__(self):
-
-        self.total_items = None # {'item': 'price'}
+        self.total_items = {}  # {'item': 'price'}
         self.total_price = 0
         self.discount = 0
 
-    def add_item(self):
-        pass
+    def add_item(self, item_price: dict):
+        self.total_items.update(item_price)
+        for key, value in item_price.items():
+            self.total_price += value
+            print(f"The {key} with the price of £{value} has been added to the cart.")
 
-    def remove_item(self):
-        pass
+    def remove_item(self, item):
+        self.total_items.pop(item)
+        print(f"{item.capitalize()} has been removed from the cart.")
 
-    def apply_discount(self):
-        pass
+    def apply_discount(self, discount_percentage: float):
+        self.discount = round((self.total_price * discount_percentage), 2)
+        print(f"The amount of £{self.discount} will be discounted from the total price.")
 
     def get_total(self):
-        pass
+        self.total_price -= self.discount
+        print(f"The total price to be paid is £{self.total_price}.")
 
     def show_items(self):
-        pass
+        print("Here are the items in the cart:")
+        for key, value in self.total_items.items():
+            print(f"{key.capitalize():15s} £{value:.2f}")
 
     def reset_register(self):
-        pass
+        self.total_items.clear()
+        self.total_price = 0
+        self.discount = 0
+        print("Cash register has been reset.")
 
 
 # EXAMPLE code run:
 
-# ADD
+register_1 = CashRegister()
+register_2 = CashRegister()
+
+# check if every function works
+register_1.add_item({"apple": 2.50})
+register_1.add_item({"mango": 3.99})
+register_1.add_item({"tomato": 1.50})
+register_1.add_item({"banana": 1.75})
+register_1.add_item({"milk": 1.00})
+register_1.remove_item("banana")
+register_1.apply_discount(0.20)
+register_1.show_items()
+register_1.get_total()
+
+# check if the cash register is reset properly
+print("-----------")
+print("-----------")
+register_1.reset_register()
+register_1.show_items()
+register_1.get_total()
+
+# check if register 2 has been initialized
+print("-----------")
+print("-----------")
+print(register_2)
 
 
-"""
--- homework-1/student.py
+++ homework-1/student.py
@@ -1,24 +1,10 @@
-"""
-
-TASK 2
-
-Write a base class to represent a student. Below is a starter code.
-Feel free to add any more new features to your class.
-As a minimum a student has a name and age and a unique ID.
-
-Create a new subclass from student to represent a concrete student doing a specialization, for example:
-Software Student and Data Science student.
-
-"""
-
-
 class Student:
 
     def __init__(self, name, age, id):
         self.name = name
         self.age = age
         self.id = id
-        self.subjects = dict()
+        self.subjects = {}
 
 
 # class CFGStudent(<should inherit from Student>)
@@ -26,3 +12,38 @@ class Student:
 #     create a method to view all subjects taken by a student
 #     create a method  (and a new variable) to get student's overall mark (use average)
 
+class CFGStudent(Student):
+
+    def __init__(self, stream, name, age, id):
+        self.stream = stream
+        super(CFGStudent, self).__init__(name, age, id)
+
+    def add_subject(self, subject_grade: dict):
+        self.subjects.update(subject_grade)
+        for subj, grade in subject_grade.items():
+            print(f"The subject {subj.capitalize()} with the grade of {grade} has been added to the list of subjects.")
+
+    def remove_subject(self, subject_name):
+        self.subjects.pop(subject_name)
+
+    def view_subjects(self):
+        print(f"Here are the subjects and the grades of {self.name} of the {self.stream} specialization:")
+        print("Subjects\tGrades")
+        for subj, grade in self.subjects.items():
+            print(f"{subj.capitalize():15s} {grade}")
+
+    def average_grade(self):
+        average = round(sum(self.subjects.values()) / len(self.subjects))
+        print(f"{self.name}'s average marks is {average}.")
+
+
+student_1 = CFGStudent("Software", "Ruth", 27, 1234)
+
+# check if every function works
+student_1.add_subject({"Python": 80})
+student_1.add_subject({"Mathematics": 75})
+student_1.add_subject({"English": 65})
+student_1.add_subject({"Chemistry": 95})
+student_1.remove_subject("Chemistry")
+student_1.view_subjects()
+student_1.average_grade()
