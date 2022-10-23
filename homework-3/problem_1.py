6ac4b25ab91e282c756d69570d7ba50a9d371ea4
ruthie02 <maryruthbongon@gmail.com>
2022-10-23 13:07:09 +0200

    Added some comments on the print statement

--- homework-3/problem_1.py
+++ homework-3/problem_1.py
@@ -5,8 +5,8 @@ def generate_phrase(characters, phrase):
     return Counter(characters) == Counter(phrase)
 
 
-print(generate_phrase("aabbccc", "cbacba"))
-print(generate_phrase("abc def ghi jkl", "abcdefg   hijkl"))
-print(generate_phrase("cfg is fun", "funcgf is "))
+print(generate_phrase("aabbccc", "cbacba")) #should be false
+print(generate_phrase("abc def ghi jkl", "abcdefg   hijkl")) #should be True
+print(generate_phrase("cfg is fun", "funcgf is ")) #should be True
 print(generate_phrase("   ", "  ")) #3 white spaces vs 2 white spaces
 print(generate_phrase("   ", "   ")) #3 white spaces vs 3 white spaces
